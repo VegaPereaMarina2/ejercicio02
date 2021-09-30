4480ff6befaff1099bd4a9f8357faa3fba6ec921
Marina Vega <mvegper995@g.educaand.es>
2021-09-30 10:12:47 +0200

    first commit

---
+++ src/finds01.js
@@ -0,0 +1,7 @@
+/*
+Todos los documentos con todo los campos en los que el campo edad es 19.
+*/
+db.col01.find({edad: 19})
+/*
+{ "_id" : ObjectId("615568175b469eb143d9995f"), "nombre" : "Pepe", "edad" : 19 }
+*/
--- src/insert01.js
+++ src/insert01.js
@@ -1,7 +1,10 @@
-//db.col01.deleteMany({})
+
 //db.col01.find()
 //load("ejemplo01.js")
 //gitignore es para que no salga la colección
+db.col01.deleteMany({})
 db.col01.insertOne({nombre : "Pepe", edad : 19})
 db.col01.insertOne({nombre : "Susana", edad : 18})
 db.col01.insertOne({nombre : "Juanma", edad : 20})
+db.col01.insertOne({nombre : "Martina", edad : 23})
+db.col01.insertOne({nombre : "Salvador", edad : 19})
